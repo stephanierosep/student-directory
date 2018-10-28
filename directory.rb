91bba863338c7d89f1dee1f952fbf381aae28015
Steph Parker <stephparker1994@gmail.com>
2018-10-28 23:23:04 +0000

    interactive menu

--- directory.rb
+++ directory.rb
@@ -1,19 +1,19 @@
 def input_students
     puts "Please enter the name of the students"
     puts "To finish, just hit return twice"
-    
+    # create an empty array
     students = []
-    
+    # get the first name
     name = gets.chomp
-    
+    #w while the name is not empty, repeat this code
     while !name.empty? do
-        
+        # add the student code to the array
         students << {name: name, cohort: :november}
         puts "Now we have #{students.count} students"
-        
+        # get another name from the user
         name = gets.chomp
     end
-    
+    # return the array of students
     students
 end
 
@@ -33,8 +33,31 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 
-
+def interactive_menu
+  students = []
+  loop do 
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    
+    selection = gets.chomp
+    
+    case selection
+    when "1"
+        students = input_students
+    when "2"
+        print_header
+        print(students)
+        print_footer(students)
+    when "9"
+        exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+    
 students = input_students
-print_header
-print(students)
-print_footer(students)
+# nothing happens until the methods are called
+
+interactive_menu
